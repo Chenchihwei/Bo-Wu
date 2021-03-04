2600f43be68ac4af0f45654f9e53cc282b479013
chenchihwei <gbcow5233@gmail.com>
2021-03-04 13:43:13 +0800

    menu script.js 拉出

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -1,22 +1,124 @@
-let menu_bar = document.getElementsByClassName('menu_bar')[0];
-let menu = document.getElementsByClassName('menu')[0];
-let menuGold = document.getElementsByClassName('menuGold')[0];
-function menu_open() {
-    let not_none = menu_bar.classList.contains("none");
-    if (not_none) {
-        menu_bar.classList.remove("none");
-        $('header').addClass('black');
-    } else {
-        menu_bar.classList.add("none");
-        $('header').removeClass('black');
-    }
-};
-menu.addEventListener('click', function () {
-    menu_open();
-})
-menuGold.addEventListener('click', function () {
-    menu_open();
-})
+// let menu_bar = document.getElementsByClassName('menu_bar')[0];
+// let menu = document.getElementsByClassName('menu')[0];
+// let menuGold = document.getElementsByClassName('menuGold')[0];
+// function menu_open() {
+//     let not_none = menu_bar.classList.contains("none");
+//     if (not_none) {
+//         menu_bar.classList.remove("none");
+//         $('header').addClass('black');
+//     } else {
+//         menu_bar.classList.add("none");
+//         $('header').removeClass('black');
+//     }
+// };
+// menu.addEventListener('click', function () {
+//     menu_open();
+// })
+// menuGold.addEventListener('click', function () {
+//     menu_open();
+// })
 
 //gold
 
+let menu_bar = document.getElementsByClassName('menu_bar')[0];
+let menuGold = document.getElementsByClassName('menuGold')[0];
+
+menuGold.addEventListener('click', function () {            
+    let not_none = menu_bar.classList.contains("none");
+
+    if (not_none) 
+    {
+        menu_bar.classList.remove("none");
+        gsap.to(".indexMenuline1", {
+            duration: .5, 
+            y: '1vh',
+            rotation:45, 
+            ease: "ease", 
+            transformOrigin:"50% 50%"}
+        );
+        gsap.to(".indexMenuline2", {
+            duration: .5, 
+            width: '4vw', 
+            rotation:-45, 
+            ease: "ease", 
+            transformOrigin:"50% 50%"},
+        );
+        gsap.to(".menu p", {
+            opacity:0},
+        );
+        gsap.to(".menu_bar", {
+            opacity:0},
+        );
+        gsap.to(".menu_bar", {
+            left : 0,
+            duration: 1,
+            opacity:1,
+            ease: "ease",}
+        );
+        gsap.to(".menuGsapLine", {
+            right: 0,
+            duration: 1,
+            ease: "ease"}
+        );
+        gsap.to(".menu_child a",{
+            opacity:1,
+            stagger: 0.3,
+            ease: "ease",
+            delay : .5}
+        );
+        gsap.to(".menu_personal_exhibits",{
+            opacity:1,
+            ease: "ease",
+            delay : 1.2}
+        );
+        gsap.to(".join",{
+            opacity:1,
+            ease: "ease",
+            delay : 1.5}
+        );
+    } else {
+        menu_bar.classList.add("none");
+        gsap.to(".indexMenuline1", {
+            duration: .5, 
+            y: '0vh',
+            rotation: 0, 
+            ease: "ease", 
+            }
+        );
+        gsap.to(".indexMenuline2", {
+            duration: .5, 
+            rotation: 0, 
+            width: '2vw', 
+            ease: "ease", 
+            },
+        );
+        gsap.to(".menu p", {
+            opacity:1 },
+        );
+        gsap.to(".menu_personal_exhibits",{
+            opacity:0,
+            ease: "ease",}
+        );
+        gsap.to(".join",{
+            opacity:0,
+            ease: "ease",}
+        );
+        gsap.to(".menuGsapLine", {
+            right: '-200vw',
+            duration: 1,
+            ease: "ease"}
+        );
+        gsap.to(".menu_child a",{
+            opacity:0,
+            stagger: 0.3,
+            ease: "ease",
+            delay : .5}
+        );
+        gsap.to(".menu_bar", {
+            opacity:0,
+            ease: "ease",}
+        );
+    };
+
+    
+});
