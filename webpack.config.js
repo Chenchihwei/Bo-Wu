d3a60efd82683f34f0db65655a993578d2c59310
chenchihwei <gbcow5233@gmail.com>
2021-03-08 22:23:56 +0800

    webpack 環境建置

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,76 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+
+const webpack  = require('webpack');
 
 //配置文件
 module.exports = {
-    entry: {
-        main: './app/assets/js/index.js',
-        aboutus: './app/assets/js/about.js'
-                   },               // 入口文件
+    entry: path.join(__dirname,'app','assets','js',index.js),               // 入口文件
+    watch:true,
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist/assets/js'),
         filename: '[name].bundle.js'
     },              // 出口文件  name -> chunkname main and about
     module: {
-        rules: [{
-            // 格式
-            test: /\.(sass|scss|css)$/,
-            //順序是由下到上 sass > css > loader
-            use: [{
-                loader: MiniCssExtractPlugin.loader,
-                options: {
-                publicPath: './dist'
-                }
-            },
-                //'style-loader', 會跟原本的衝突 
-                'css-loader',
-                'sass-loader'
+        rules: [
+        //babel loader
+        {
+            // test:/.jsx?$/,
+            test: /\.(js)$/,
+            include:[
+                path.resolve(__dirname,'app')
             ],
-        }]
+            // exclude: /(node_modules)/,
+            exclude:[
+                path.resolve(__dirname,'node_modules')
+            ],
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }],
+        },
+    ]
 
     },  // 處裡對應模組
+    resolve:{
+        extensions:['.json','.js','.jsx'],
+        alias:{
+            vue:'vue/dist/vue.js '
+        }
+    },
     plugins: [
-        new MiniCssExtractPlugin({
-            filename: "./[name].css" //output 檔案名稱
-        }),
-        new HtmlWebpackPlugin({
-            chunks : ['main'],  //選擇注入資源 chunk
-            inject  : 'body', //預設<body> js </body>  head or body
-            template : './src/index.html', 
-            filename : 'index.html' //目的地
-        })
+         //清理舊的檔案
+        new CleanWebpackPlugin(),
+        //css
+        // new MiniCssExtractPlugin({
+        //     filename: "./[name].css" //output 檔案名稱
+        // }),
+        // new HtmlWebpackPlugin({
+        //     chunks : ['main'],  //選擇注入資源 chunk
+        //     inject  : 'body', //預設<body> js </body>  head or body
+        //     template : './src/index.html', 
+        //     filename : 'index.html' //目的地
+        // }),
+         //全域載入jq
+         new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+          })
     ],   // 對應的插件
-    // devServer: {},           // 服務器配置
+    devtool:'source-map',
+    devServer: {
+        contentBase: path.join(__dirname,'./dist'),
+        host: 'localhost',
+        port: 3000,
+        // 指定首頁檔案
+        index: 'index.html',
+        open: true
+    },           // 服務器配置
     mode: 'development'      // 開發模式配置
 }
