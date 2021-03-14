37b8203524f142637415113947a04096494b11d6
henrychen <henry4968@yahoo.com.tw>
2021-03-14 18:00:12 +0800

    松輝部分

--- app/assets/js/postcard.js
+++ app/assets/js/postcard.js
@@ -1,155 +1,277 @@
 //自動生成圖片
 window.onload = background();
 
-function background(){
+function background() {
   $.ajax({
     method: "POST",
-    url: "php/front/cardImg.php",
+    url: "./assets/php/front/cardImg.php",
     data: {
       'postcard_category': 1,
     },
     dataType: "text",
     success: function (response) {
-        //更新html內容
+      //更新html內容
       // console.log(response);
       let postImg = document.getElementsByClassName('post_img')[0].querySelector('ul');
-      postImg.innerHTML=response;
+      postImg.innerHTML = response;
       changePic();
     },
     error: function (exception) {
-        alert("發生錯誤: " + exception.status);
+      alert("發生錯誤: " + exception.status);
     }
   });
   //換背景圖片
-  function changePic(){
+  function changePic() {
     let changeImg = document.getElementsByClassName('post_img')[0].querySelectorAll('img');
     let cardImg = document.getElementsByClassName('postcard_bg')[0];
-    for(let i = 0 ; i < changeImg.length ; i++){
-      changeImg[i].addEventListener('click',function(){
+    for (let i = 0; i < changeImg.length; i++) {
+      changeImg[i].addEventListener('click', function () {
         let imgSrc = changeImg[i].getAttribute('src');
-        cardImg.style.backgroundImage="url('"+imgSrc+"')";
+        cardImg.style.backgroundImage = "url('" + imgSrc + "')";
       })
     }
-  }  
+  }
 }
-function logo(){
+function logo() {
   $.ajax({
     method: "POST",
-    url: "php/front/cardImg.php",
+    url: "./assets/php/front/cardImg.php",
     data: {
       'postcard_category': 2,
     },
     dataType: "text",
     success: function (response) {
-        //更新html內容
+      //更新html內容
       // console.log(response);
       let postImg = document.getElementsByClassName('post_img')[0].querySelector('ul');
-      postImg.innerHTML=response;
+      postImg.innerHTML = response;
       dragDrop();
     },
     error: function (exception) {
-        alert("發生錯誤: " + exception.status);
+      alert("發生錯誤: " + exception.status);
     }
   });
 }
 //drop and drag 素材
-function dragDrop(){
+function dragDrop() {
   let img = document.getElementsByClassName('post_img')[0].querySelectorAll('img');
-  for(let i = 0;i<img.length;i++){
-    img[i].addEventListener('dragstart',function(e){
-      e.dataTransfer.setData('image/png',`<img src="${img[i].src}" alt>`);
+  for (let i = 0; i < img.length; i++) {
+    img[i].addEventListener('dragstart', function (e) {
+      e.dataTransfer.setData('image/png', `<img class='drag material' src="${img[i].src}" alt>`);
     });
   }
   box = document.getElementsByClassName('postcard_bg')[0];
-  box.addEventListener('dragover',function(e){e.preventDefault();});
-  box.addEventListener('drop',function dropped(e){
+  box.addEventListener('dragover', function (e) { e.preventDefault(); });
+  box.addEventListener('drop', function dropped(e) {
     e.preventDefault();
     console.log(e.dataTransfer.getData('image/png'))
-    console.log(typeof(e.dataTransfer.getData('image/png')))
-    box.innerHTML += e.dataTransfer.getData('image/png');
+    console.log(typeof (e.dataTransfer.getData('image/png')));
+    // box.innerHTML += e.dataTransfer.getData('image/png');  //用innerHTML會蓋掉原有的內容
+    box.insertAdjacentHTML('beforeend', e.dataTransfer.getData('image/png'));
+    drag();
   })
 }
 //文字框 CLICK 出現 
-function message(){
+function message() {
   let postcard_message = document.getElementsByClassName('postcard_message')[0];
-  if(postcard_message.style.opacity == 0){
+  if (postcard_message.style.opacity == 0) {
     postcard_message.style.opacity = 1;
-  }else{
+  } else {
     postcard_message.style.opacity = 0;
   }
 }
 //message saved and 創造
-function messageSave(){
-  let value = document.getElementsByClassName('pos_textbox')[0].value;
+function messageSave() {
+  let pos_textbox = document.getElementsByClassName('pos_textbox')[0];
   let span = document.createElement("span");
   let textarea = document.getElementsByTagName('textarea')[0];
   span.classList.add('drag');
   span.style.color = "white";
   span.style.fontSize = textarea.style.fontSize;
   span.style.color = textarea.style.color;
-  span.style.position= 'absolute';
+  span.style.position = 'absolute';
   span.style.top = '50%';
-  span.style.right='50%';
-  span.innerHTML = value;
+  span.style.right = '50%';
+  span.innerHTML = pos_textbox.value;
   let postcard_bg = document.getElementsByClassName('postcard_bg')[0];
   postcard_bg.appendChild(span);
+  textarea.value = "";
   drag();
+
 }
 // message 自由拖拉
-function drag(){
-  $(document).ready(function(){
+function drag() {
+  $(document).ready(function () {
     $('.drag').draggable({
-        cursor: 'pointer',
-        opacity: 0.6,
-        containment:'parent',
-        scroll: false,
+      cursor: 'pointer',
+      opacity: 0.6,
+      containment: 'parent',
+      scroll: false,
     });
   });
 }
-function messageDel(){
-
+//訊息點擊外框
+document.addEventListener('click', function outline(e) {
+  if (e.target.classList.contains('drag')) {
+    if (e.target.classList.contains('outline')) {
+      e.target.classList.remove('outline')
+    } else {
+      e.target.classList.add('outline');
+    }
+  }
+})
+//訊息刪除
+function messageDel(e) {
+  let mesSelect = document.querySelectorAll('.outline');
+  [...mesSelect].forEach(item => item.remove());
 }
 //改變大小
-function changeLineWidth (e) {
+function changeLineWidth(e) {
+  //點選可更改
+  let postcard_bg = document.getElementsByClassName('postcard_bg')[0];
+  let span = postcard_bg.getElementsByTagName('span')[1];
+  let outline = document.getElementsByClassName('drag');
   let newWidth = document.getElementsByClassName('input-range')[0].value;
-  let textarea = document.getElementsByTagName('textarea')[0];
-  textarea.style.fontSize = newWidth + 'px';
+  if (postcard_bg.contains(span)) {
+    for (let i = 0; i < outline.length; i++) {
+      if (outline[i].classList.contains('outline')) {
+        let mesSelect = document.querySelectorAll('.outline');
+        [...mesSelect].forEach(item => item.style.fontSize = newWidth + 'px');
+      } else {//僅輸入框更改
+        let textarea = document.getElementsByTagName('textarea')[0];
+        textarea.style.fontSize = newWidth + 'px';
+      }
+    }
+  } else {
+    let textarea = document.getElementsByTagName('textarea')[0];
+    textarea.style.fontSize = newWidth + 'px';
+  }
 }
 //改變顏色
-function changeLineColor () {
+function changeLineColor() {
+  let postcard_bg = document.getElementsByClassName('postcard_bg')[0];
+  let span = postcard_bg.getElementsByTagName('span')[1];
+  let outline = document.getElementsByClassName('drag');
   let newColor = document.getElementById('shadowColorInput').value;
-  let textarea = document.getElementsByTagName('textarea')[0];
-  textarea.style.color = newColor;
+  if (postcard_bg.contains(span)) {
+    for (let i = 0; i < outline.length; i++) {
+      if (outline[i].classList.contains('outline')) {
+        let mesSelect = document.querySelectorAll('.outline');
+        [...mesSelect].forEach(item => item.style.color = newColor);
+      } else {//僅輸入框更改
+        let textarea = document.getElementsByTagName('textarea')[0];
+        textarea.style.color = newColor;
+      }
+    }
+  } else {
+    let textarea = document.getElementsByTagName('textarea')[0];
+    textarea.style.color = newColor;
+  }
 }
-//canvas
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-canvas.style.width = '35.4583vw'; 
-canvas.style.height = '20.8333vw'; 
-canvas.style.zIndex='10000';
+//canvas 畫板
+$('.fa-pen').click(function () {
+  let markerWidth = 1;
+  let markerColor = 'black';
+  let lastEvent;
+  let mouseDown = false;
 
-function getMousePos(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
-}
+  let canvas = $('canvas');
 
-function mouseMove(evt) {
-  var mousePos = getMousePos(canvas, evt);
-  ctx.lineTo(mousePos.x, mousePos.y);
-  ctx.stroke();
-}
+  let context = $('canvas')[0].getContext('2d');
 
-canvas.addEventListener('mousedown', function(evt) {
-  var mousePos = getMousePos(canvas, evt);
-  ctx.beginPath();
-  ctx.moveTo(mousePos.x, mousePos.y);
-  evt.preventDefault();
-  canvas.addEventListener('mousemove', mouseMove, false);
+  canvas.mousedown(function (e) {
+    lastEvent = e;
+    mouseDown = true;
+  }).mousemove(function (e) {
+    if (mouseDown) {
+
+      context.beginPath();
+
+      context.moveTo(lastEvent.offsetX, lastEvent.offsetY);
+      context.lineTo(e.offsetX, e.offsetY);
+      context.lineWidth = markerWidth;
+      context.strokeStyle = markerColor;
+      context.lineCap = 'round';
+      context.stroke();
+
+      lastEvent = e;
+
+    }
+  }).mouseup(function () {
+    mouseDown = false;
+  });
+
+  var changeWidth = function () {
+    markerWidth = $('.input-range').val();
+  }
+  var changeColor = function () {
+    markerColor = $('#shadowColorInput').val();
+  }
+
+  var clear = function () {
+    context.clearRect(0, 0, 690, 390);
+  }
+
+  $('.fa-eraser').on('click', clear);
+
+  $('.input-range').change(changeWidth);
+
+  $('#shadowColorInput').change(changeColor);
+
+
 });
 
-canvas.addEventListener('mouseup', function() {
-  canvas.removeEventListener('mousemove', mouseMove, false);
-}, false);
+//html2canvas 下載圖片
+function screenshot() {
+  $(".postcard_send").css("height", "");
+  // $(".postcard_send").css("width", "753px");
+  $("html, body").scrollTop(0);
+  html2canvas(document.getElementsByClassName('postcard_send')[0]).then(function (canvas) {
+    // document.body.appendChild(canvas);
+    var a = document.createElement('a');
+    a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+    a.download = 'image.jpg';
+    a.click();
+  });
+}
+
+$('.send').click(function () {
+  $(".postcard_send").css("height", "");
+  $(".postcard_send").css("width", "753px");
+  $("html, body").scrollTop(0);
+  html2canvas(document.getElementsByClassName('postcard_send')[0]).then(function (canvas) {
+    // document.body.appendChild(canvas);
+    var a = document.createElement('a');
+    a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+    let base64 = canvas.toDataURL("image/jpeg");
+    // console.log(base64);
+    let your_mail = $('.email_text').val();
+    mailtest(your_mail, base64);
+
+  });
+});
+
+
+function mailtest(your_mail, base64) {
+
+  //Typing your code...
+  $.ajax({
+    method: "POST",
+    url: "./assets/php/front/send.php",
+    data: {
+      'your_mail': your_mail,
+      'base64': base64,
+    },
+    dataType: "text",
+    success: function (response) {
+      //更新html內容
+      // document.getElementsByClassName('tr')[0].innerHTML = response;
+      console.log(response);
+    },
+    error: function (exception) {
+      alert("發生錯誤: " + exception.status);
+    }
+  });
+
+}
+
--- app/assets/php/front/cardImg.php
+++ app/assets/php/front/cardImg.php
@@ -1,12 +1,15 @@
 <?php
-    include("../Lib/UtilClass.php");
-    $Util = new UtilClass();
+    // include("../Lib/UtilClass.php");
+    // $Util = new UtilClass();
+    include('../Conn.php');
+    
 
     //建立SQL
     $sql = "SELECT * FROM postcard WHERE postcard_exhibition = 1 and postcard_category = ?";
 
     //執行
-    $statement = $Util->getPDO()->prepare($sql);
+    // $statement = $Util->getPDO()->prepare($sql);
+    $statement = $pdo->prepare($sql);
     $statement->bindValue(1, $_POST["postcard_category"]);
     $statement->execute();
     $data = $statement->fetchAll();
