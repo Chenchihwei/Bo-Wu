d7ff896e498999f0f3ec524f398bf47d335021fc
henrychen <henry4968@yahoo.com.tw>
2021-03-16 01:00:56 +0800

    vue修理

--- app/assets/php/front/work_ocean.php
+++ app/assets/php/front/work_ocean.php
@@ -15,7 +15,7 @@ include('../Conn.php');
 //  $dsn = "mysql:host=".$db_host.";dbname=".$db_select;
 
 //  //建立PDO物件，並放入指定的相關資料
- $pdo = new PDO($dsn, $db_user, $db_pass);    
+//  $pdo = new PDO($dsn, $db_user, $db_pass);    
 
   $sql = 'SELECT w.*, c.collection_id, c.collection_subject
         FROM 
