29d172a068f657418f03cfb499712b06ae109836
shang-jui <hn15637648@gmail.com>
2021-03-15 22:11:34 +0800

    no message

---
+++ app/assets/php/front/highlevel_member_count.php
@@ -0,0 +1,34 @@
+<?php
+    include('../Conn.php');
+
+    $member_id = $_POST['member_id'];
+
+
+    $sql = 'SELECT w.*, count(wm.work_id), m.*  
+    from work w
+        left join work_message wm
+        on w.work_id = wm.work_id
+        join member m
+        on w.member_id = m.member_id
+      where m.member_id = ?
+      group by w.work_id';
+    
+    
+    
+    
+    $statement = $pdo->prepare($sql);    
+    $statement->bindValue(1, $member_id);      
+    $statement->execute();  
+
+    $goodTotal = '';
+    $peopleTotal='';
+    $commentTotal='';
+
+    $data = $statement->fetchAll();
+    foreach($data as $index => $row){
+      $goodTotal +=  $row['like_num'];
+      $peopleTotal +=  $row['visitors'];
+      $commentTotal +=  $row['count(wm.work_id)'];
+    };
+    echo $goodTotal.'//'.$peopleTotal.'//'.$commentTotal;
+?>
